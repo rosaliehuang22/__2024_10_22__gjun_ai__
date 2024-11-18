f4177a9f340aa61719bce182b432d33d533ecf68
rosaliehuang22 <132977133+rosaliehuang22@users.noreply.github.com>
2024-11-18 02:36:14 +0000

    lesson8_1

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,12 +1,24 @@
 import random
 def get_names(nums:int=2)->list[str]:
-    with open('names.txt', encoding='utf-8', mode='r') as file:
-        names_str= file.read()
+    with open('/workspaces/__2024_10_22__gjun_ai__/lesson8/names.txt',encoding='utf-8',mode='r') as file:
+        names_str = file.read()
     names:list[str] = names_str.split(sep='\n')
     names = random.choices(names,k=nums)
     return names
 
-nums = int(input('請輸入學生數量(最多10位): '))
+def generate_students(names:list[str]) -> list[dict]:
+    students:list[dict] = []
+    for name in names:
+        chinese = random.randint(50, 100)
+        english = random.randint(50, 100)
+        math = random.randint(50, 100)
+        student = {'name':name,'chinese':chinese,'english':english,'math':math}
+        students.append(student)
+    return students  
+    
+
+
+nums = int(input('請輸入學生數量(最多10位):'))
 student_names:list[str] = get_names(nums=nums)
-for name in student_names:
-    print(name)
+students:list[dict] = generate_students(names=student_names)
+print(students)
